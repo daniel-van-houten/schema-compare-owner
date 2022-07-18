46491696565bcf8c9824efa1681d099ca8253a8d
danielvanhouten <daniel.vanhouten87@gmail.com>
2022-07-18 16:15:58 -0500

    Adding initial project and migration.

---
+++ ExampleDatabase/AddGuitarsTable.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+
+namespace ExampleDatabase
+{
+    [Migration(1)]
+    public class AddGuitarsTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("guitars")
+                .WithColumn("guitar_id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("brand").AsString()
+                .WithColumn("model").AsString()
+                .WithColumn("string_count").AsInt32()
+                .WithColumn("body_style").AsString()
+                .WithColumn("scale_length").AsFloat();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("guitars");
+        }
+    }
+}
---
+++ ExampleDatabase/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+using FluentMigrator.Runner;
+using FluentMigrator.Runner.Initialization;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExampleDatabase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = CreateServices();
+
+            // Put the database update into a scope to ensure
+            // that all resources will be disposed.
+            using (var scope = serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
+        }
+
+        /// <summary>
+        /// Configure the dependency injection services
+        /// </summary>
+        private static IServiceProvider CreateServices()
+        {
+            return new ServiceCollection()
+                // Add common FluentMigrator services
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    // Add SQLite support to FluentMigrator
+                    .AddPostgres()
+                    // Set the connection string
+                    .WithGlobalConnectionString("Server=localhost; Port=5438; Database=example; User ID=postgres; Password=postgres")
+                    // Define the assembly containing the migrations
+                    .ScanIn(typeof(AddGuitarsTable).Assembly).For.Migrations())
+                // Enable logging to console in the FluentMigrator way
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                // Build the service provider
+                .BuildServiceProvider(false);
+        }
+
+
+        /// <summary>
+        /// Update the database
+        /// </summary>
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            // Instantiate the runner
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            // Execute the migrations
+            runner.MigrateUp();
+        }
+    }
+}
